1f3d936d5ca5853bb7f144293e73687ab061bdc9
201314292 <sergiocubur@gmail.com>
2022-03-10 01:18:58 -0600

    x
    x

--- creds_template.js
+++ creds_template.js
@@ -1,8 +1,8 @@
 let aws_keys = {
     s3: {
-        region: 'us-east-2',
-        accessKeyId: "",
-        secretAccessKey: "",
+        region: 'us-east-1',
+        accessKeyId: 'AKIA3777HMDJA64H43BI',
+        secretAccessKey: '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'
         //apiVersion: '2006-03-01',
     },
     dynamodb: {
--- db_creds.template.js
+++ db_creds.template.js
@@ -2,7 +2,7 @@ let db_credentials = {
     host: "dbsemione.c03odn6dxvh2.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "admin",
-    password: "",
+    password: "Seminario1*",
     database: "dbsemione"
 }
 module.exports = db_credentials;
